4df2d7a55a5cb34eaf84a83065be6ea0dbed6a3c
Arturo Rodas <arturo.rodas.gonzales@gmail.com>
2025-08-22 20:51:04 -0500

    test2

--- src/undar/lib/globales.js
+++ src/undar/lib/globales.js
@@ -1,8 +1,8 @@
-export const API_URL = 'http://localhost:3000'
+export const API_URL = 'http://apps.local.openedx.io:3000';
 
 export const keysLocalStorage = {
   usuario: 'usuario',
-}
+};
 
 export const states = {
   Activo: 'Activo',
@@ -11,14 +11,14 @@ export const states = {
   Suspendido: 'Suspendido',
   Inactivo: 'Inactivo',
   Finalizado: 'Finalizado',
-}
+};
 
 export const tiposExamen = {
   Sync: 'Sync',
   Async: 'Async',
-}
+};
 
 export const tipoNivelDelogro = {
   Porcentaje: 'Porcentaje',
   Rango: 'Rango',
-}
+};
